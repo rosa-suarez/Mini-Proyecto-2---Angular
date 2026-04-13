249a1cab151c92204033ea79b9e09639b2ac66a5
Vanessa Carolina Torres Rojas <torres100vane124@gmail.com>
2026-04-12 19:12:22 -0600

    cambio a postgresql

--- backend-tienda/index.js
+++ backend-tienda/index.js
@@ -2,24 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 
+const { Pool } = require('pg');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 /* CONEXIÓN MYSQL */
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tienda_bolsas'
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'tienda_bolsas'
+// });
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-db.connect(err => {
+pool.connect(err => {
     if (err) {
         console.log('Error conexión:', err);
     } else {
-        console.log('Conectado a MySQL');
+        console.log('Conectado a PostgreSQL');
     }
 });
 

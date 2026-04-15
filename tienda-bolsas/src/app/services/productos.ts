3f06ce182646a1952e1df154cff08d6141c78c0b
Vanessa Carolina Torres Rojas <torres100vane124@gmail.com>
2026-04-14 22:59:04 -0600

    proyecto completo front + back

--- tienda-bolsas/src/app/services/productos.ts
+++ tienda-bolsas/src/app/services/productos.ts
@@ -6,8 +6,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductosService {
 
-  private API = 'http://localhost:3000';
-
+  // private API = 'http://localhost:3000';
+  private API = 'https://mini-proyecto-2-angular.onrender.com';
+  
   constructor(private http: HttpClient) {}
 
   getProductos() {
